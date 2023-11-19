ae78dde3610aa1379792ae5ded3fb8d6dc735eca
buimanhdat09 <datbmph32794@fpt.edu.vn>
2023-11-19 14:19:58 +0700

    quanr lis danh mucj sanr phaam

---
+++ mdel/danhmuc.php
@@ -0,0 +1,27 @@
+<?php 
+function insert_danhmuc($namedm){
+  
+    $sql = "insert into danhmuc(namedm) values('$namedm')";
+    pdo_execute($sql);
+}
+
+function delete_danhmuc($id){
+    $sql = "delete from danhmuc where id=" . $id;
+    pdo_query($sql);
+
+}
+function loadall_danhmuc(){
+    $sql = "select * from danhmuc order by id desc";
+    $listdanhmuc = pdo_query($sql);
+    return $listdanhmuc;
+}
+function loadone_danhmuc($id){
+    $sql = "select * from danhmuc where id =".$id;
+    $dm=pdo_query_one($sql);
+    return $dm;
+}
+function update_danhmuc($id,$namedm){
+    $sql = "update danhmuc set namedm='".$namedm."' where id=".$id;
+                pdo_execute($sql);
+}
+?>
