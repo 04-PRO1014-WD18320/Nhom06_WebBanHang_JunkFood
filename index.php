4bc9ff72c76f9ba1a0581001ca3bfbec48ddc58e
duph39199 <dutvph39199@fpt.edu.vn>
2023-11-19 15:48:55 +0700

    test lan 1

--- index.php
+++ index.php
@@ -4,19 +4,22 @@
     include "mdel/danhmuc.php";
     include "mdel/sanpham.php";
     include "mdel/taikhoan.php";
+    include "./view/header.php";
+
+
     
     if((isset($_GET['act']))&&($_GET['act']!="")){
         $act=$_GET['act'];
         switch ($act) {
             case 'lienhe':
-                include "view/home.php";
+                include "./view/home.php";
                 break;
             default:
-                include "view/home.php";
+            include "./view/home.php";
                 break;
             }
         }else{
-            include "view/home.php";
+            include "./view/home.php";
         }
-    include "view/footer.php";
+        include "./view/footer.php";
 ?>
