11d43901ebdf60b693c349f275b4bd25705c1bd2
buimanhdat09 <datbmph32794@fpt.edu.vn>
2023-11-21 14:35:40 +0700

    css admin

--- admin/header.php
+++ admin/header.php
@@ -1,5 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="./icons/fontawesome-free-6.4.2-web/js/all.min.js"></script>
+    <link rel="stylesheet" href="./longcss/styles.css">
+    <link rel="stylesheet" href="../view/datcss/datcss.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="logo">
+                <img src="../images/logo.jpg" width="80" height="80" alt="">
+            </div>
+            <div class="menu">
+                <ul>
+                <li><a href="index.php">Trang chu</a></li>
+                <li><a href="index.php?act=adddm">DANH MỤC</a></li>
+                <li><a href="index.php?act=addsp">SẢN PHẨM</a></li>
+                <li><a href="index.php?act=dskh">KHÁCH HÀNG</a></li>
+                <li><a href="index.php?act=dsbl">BÌNH LUẬN</a></li>
+                <li><a href="index.php?act=thongke">THỐNG KÊ</a></li>
+                </ul>
+            </div>
+            <div class="search">
+                <div class="input">
+                    <input type="text" value="search">
+                </div>
+                <div class="iconsearch">
+                    <a href=""><i class="fa-solid fa-magnifying-glass"></i></a>
+                </div>
+            </div>
+            <div class="icons">
+                <a href="index.php?act=dangnhap"><i class="fa-solid fa-user"></i></a>
+                <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
+            </div>
+        </div>
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -22,4 +63,4 @@
                 <li><a href="index.php?act=dsbl">BÌNH LUẬN</a></li>
                 <li><a href="index.php?act=thongke">THỐNG KÊ</a></li>
             </ul>
-        </div>
+        </div> -->
