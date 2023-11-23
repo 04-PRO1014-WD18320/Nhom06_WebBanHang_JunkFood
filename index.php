0789a33ff93ac8f1d528b1e9b5ac21a685e41246
longnhph44749@fpt.edu.vn <longxahoi7>
2023-11-23 15:33:25 +0700

    alo ae

--- index.php
+++ index.php
@@ -1,22 +1,28 @@
 <?php
 session_start();
-include "./view/header.php";
-include "./view/home.php";
-include "./view/footer.php";
-include "./mdel/pdo.php";
+include "view/header.php";
 include "mdel/pdo.php";
 include "mdel/danhmuc.php";
 include "mdel/sanpham.php";
 include "mdel/taikhoan.php";
-
+$listsp=loadall_sanpham_home();
+$listspmin=loadall_sanpham_soluongmin();
 if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
     $act = $_GET['act'];
     switch ($act) {
-        case 'lienhe':
-            include "view/home.php";
+        case "sanphamchitiet":
+            if(isset($_GET["idsp"])&& ($_GET["idsp"])>0){
+                $listspct= loadone_sanpham($_GET["idsp"]);
+                extract($listspct);
+                $spcl=load_sanpham_cungloai($id,$iddm);
+                include "view/chitietsanpham.php";
+            }else{
+                echo "Lỗi to đùng";
+                include "view/home.php";
+            }
             break;
+
         default:
-            include "view/home.php";
             break;
     }
 } else {
--- mdel/sanpham.php
+++ mdel/sanpham.php
@@ -11,10 +11,12 @@ function delete_sanpham($id){
     pdo_query($sql);
 }
 function loadall_sanpham_home(){
- 
     $sql ="select * from sanpham where 1 order by id desc limit 0,9";
-    
-   
+    $listsanpham = pdo_query($sql);
+    return    $listsanpham;
+}
+function loadall_sanpham_soluongmin(){
+    $sql ="select * FROM sanpham where soluong < 20";
     $listsanpham = pdo_query($sql);
     return    $listsanpham;
 }
